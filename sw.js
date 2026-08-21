01fd0b3096fbc1437253605fdbf60661cfa52eb4
marmarok <200153299+marmarok@users.noreply.github.com>
2026-08-21 12:36:14 +0300

    Tamir kılavuzunda sürüm yolu düzeltildi

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 
 // DİKKAT: her yayında bu sürüm değişmeli, yoksa telefonlar eski dosyaları
 // önbellekten sunmaya devam eder. arac/yayinla.sh bunu kendiliğinden günceller.
-const SURUM = 'gerok-20260821-122706';
+const SURUM = 'gerok-20260821-123611';
 
 const DOSYALAR = [
   './',
